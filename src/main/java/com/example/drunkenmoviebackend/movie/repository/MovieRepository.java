e4559c4a036bad554a248a1077353b75536602aa
KIMSIHUN <tlgns14@nate.com>
2025-12-24 17:25:05 +0900

    🎨db 커넥션, movie 컨트롤러 추가

--- src/main/java/com/example/drunkenmoviebackend/movie/repository/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.drunkenmoviebackend.movie.repository;
-
-import com.example.drunkenmoviebackend.movie.domain.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
